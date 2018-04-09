d6c4c975215483a78cd546d12b8fb0d27a914f3e
VadimBolokhov <wad4@inbox.ru>
2018-04-09 13:09:30 +0300

    Используя контейнер на базе связанного списка создать контейнер Stack и Queue. [#160]

--- chapter_004/src/test/java/ru/job4j/list/SimpleStackTest.java
+++ chapter_004/src/test/java/ru/job4j/list/SimpleStackTest.java
@@ -2,10 +2,6 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
