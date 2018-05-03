fba1407193c570a448ad48ce51411addf652def3
VadimBolokhov <wad4@inbox.ru>
2018-05-03 16:48:09 +0300

    Реализовать механизм программнной остановки потока. [#1019]

--- chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -15,6 +15,8 @@ public class RectangleMove implements Runnable {
     private double velocityX = 1.0;
     /** Скорость движения по вертикали */
     private double velocityY = -0.8d;
+    /** Флаг остановки */
+    private boolean stop = false;
 
     /**
      * Конструктор - создание объекта с заданным параметром
@@ -24,9 +26,16 @@ public class RectangleMove implements Runnable {
         this.rect = rect;
     }
 
+    /**
+     * Устанавливает {@code true} для флага остановки
+     */
+    public void setStop() {
+        this.stop = true;
+    }
+
     @Override
     public void run() {
-        while (true) {
+        while (!stop) {
             this.rect.setX(this.rect.getX() + this.velocityX);
             this.rect.setY(this.rect.getY() + this.velocityY);
             if (this.rect.getX() > 300 || this.rect.getX() < 0) {
