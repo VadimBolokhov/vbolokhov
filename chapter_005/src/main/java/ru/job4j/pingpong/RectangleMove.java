63ba55ba79d0b2e298f86797796f925dcabc0420
VadimBolokhov <wad4@inbox.ru>
2018-05-03 15:20:46 +0300

    1. Пин-понг. [#1016]

---
+++ chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -0,0 +1,45 @@
+package ru.job4j.pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+/**
+ * Движение четырёхугольника в окне.
+ * @author Vadim Bolokhov
+ * @version $Id$
+ * @since 0.1
+ */
+public class RectangleMove implements Runnable {
+    /** Четырёхугольная фигура */
+    private final Rectangle rect;
+    /** Скорость движения по горизонтали */
+    private double velocityX = 1.0;
+    /** Скорость движения по вертикали */
+    private double velocityY = -0.8d;
+
+    /**
+     * Конструктор - создание объекта с заданным параметром
+     * @param rect четырёхугольник
+     */
+    public RectangleMove(final Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.rect.setX(this.rect.getX() + this.velocityX);
+            this.rect.setY(this.rect.getY() + this.velocityY);
+            if (this.rect.getX() > 300 || this.rect.getX() < 0) {
+                this.velocityX = -this.velocityX;
+            }
+            if (this.rect.getY() > 300 || this.rect.getY() < 0) {
+                this.velocityY = -this.velocityY;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
