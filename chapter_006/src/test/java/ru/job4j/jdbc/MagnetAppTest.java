908585dff0ca95777ea99b29747242ea5a51a7cd
VadimBolokhov <wad4@inbox.ru>
2018-08-02 15:13:26 +0300

    XML XSLT JDBC Оптимизация [#20459]

---
+++ chapter_006/src/test/java/ru/job4j/jdbc/MagnetAppTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.jdbc;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * MagnetApp Test.
+ * @author Vadim Bolokhov
+ * @version $Id$
+ * @since 0.1
+ */
+public class MagnetAppTest {
+
+    @Test
+    public void whenRunApplicationWithArgumentFourThenReturnTen() throws Exception {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        MagnetApp.main(new String[] {String.valueOf(4)});
+        int result = Integer.parseInt(new String(out.toByteArray()).trim());
+        assertThat(result, is(10));
+        System.setOut(stdout);
+    }
+}
